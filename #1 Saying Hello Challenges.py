3d99f26f24f11304e9ebb0827cf44aa005ab786d
Jack Alden <50923508+swimbikerun96@users.noreply.github.com>
2023-03-04 21:42:37 -0500

    Delete #1 Saying Hello Challenges.py

--- #1 Saying Hello Challenges.py
+++
@@ -1,8 +0,0 @@
-#Create List for storing the input to be collected
-name_list = []
-
-#Handle Input
-name_list.append(input("What is your name? "))
-
-#Produce Output
-print(f'Hello, {name_list[0]}, nice to meet you!')
