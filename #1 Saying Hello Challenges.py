88b5dcdc12ee2bf7acf24223d03ce6b8baa8282c
Jack Alden <50923508+swimbikerun96@users.noreply.github.com>
2023-03-04 21:37:30 -0500

    Add files via upload

---
+++ #1 Saying Hello Challenges.py
@@ -0,0 +1,8 @@
+#Create List for storing the input to be collected
+name_list = []
+
+#Handle Input
+name_list.append(input("What is your name? "))
+
+#Produce Output
+print(f'Hello, {name_list[0]}, nice to meet you!')
